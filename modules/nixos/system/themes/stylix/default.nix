f2ebc32181a95b89bfbde9fe7ca1c71f856c4544
Cody <acodywright@gmail.com>
2025-07-31 17:50:09 -0400

    restructure

--- modules/nixos/system/themes/stylix/default.nix
+++ modules/nixos/system/themes/stylix/default.nix
@@ -26,8 +26,121 @@ let
 in
 {
   options.${namespace}.module = with types; {
-    enable = mkBoolOpt false "Enable module";
+    enable = mkBoolOpt false ''
+      Whether to enable Stylix theming.
+      
+      When enabled, this module will:
+      - Configure Stylix with a dark theme (Gruvbox)
+      - Set up custom fonts (JetBrains Mono, DejaVu, Noto Emoji)
+      - Configure cursor theme (Bibata Modern Ice)
+      - Apply theming to the entire system
+      
+      Example:
+      ```nix
+      FTS-FLEET = {
+        system.themes.stylix = enabled;
+      };
+      ```
+    '';
+    
+    base16Scheme = mkOpt str "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml" ''
+      The base16 color scheme to use for theming.
+      Defaults to Gruvbox dark medium theme.
+    '';
+    
+    image = mkOpt (either path str) null ''
+      Path to the wallpaper image.
+      If null, no wallpaper will be set.
+    '';
+    
+    cursor = {
+      package = mkOpt package pkgs.bibata-cursors ''
+        The cursor package to use.
+      '';
+      name = mkOpt str "Bibata-Modern-Ice" ''
+        The name of the cursor theme.
+      '';
+      size = mkOpt int 24 ''
+        The size of the cursor.
+      '';
+    };
+    
+    fonts = {
+      monospace = {
+        package = mkOpt package pkgs.nerd-fonts.jetbrains-mono ''
+          The monospace font package.
+        '';
+        name = mkOpt str "JetBrainsMono Nerd Font Mono" ''
+          The name of the monospace font.
+        '';
+      };
+      sansSerif = {
+        package = mkOpt package pkgs.dejavu_fonts ''
+          The sans-serif font package.
+        '';
+        name = mkOpt str "DejaVu Sans" ''
+          The name of the sans-serif font.
+        '';
+      };
+      serif = {
+        package = mkOpt package pkgs.dejavu_fonts ''
+          The serif font package.
+        '';
+        name = mkOpt str "DejaVu Serif" ''
+          The name of the serif font.
+        '';
+      };
+      emoji = {
+        package = mkOpt package pkgs.noto-fonts-emoji ''
+          The emoji font package.
+        '';
+        name = mkOpt str "Noto Color Emoji" ''
+          The name of the emoji font.
+        '';
+      };
+    };
+    
+    polarity = mkOpt (enum [ "light" "dark" ]) "dark" ''
+      The polarity of the theme (light or dark).
+    '';
   };
 
-  config = mkIf cfg.enable { };
+  config = mkIf cfg.enable {
+    # Import Stylix module
+    imports = [ inputs.stylix.nixosModules.stylix ];
+    
+    # Configure Stylix
+    stylix = {
+      enable = true;
+      base16Scheme = cfg.base16Scheme;
+      image = cfg.image;
+      
+      cursor = {
+        package = cfg.cursor.package;
+        name = cfg.cursor.name;
+        size = cfg.cursor.size;
+      };
+      
+      fonts = {
+        monospace = {
+          package = cfg.fonts.monospace.package;
+          name = cfg.fonts.monospace.name;
+        };
+        sansSerif = {
+          package = cfg.fonts.sansSerif.package;
+          name = cfg.fonts.sansSerif.name;
+        };
+        serif = {
+          package = cfg.fonts.serif.package;
+          name = cfg.fonts.serif.name;
+        };
+        emoji = {
+          package = cfg.fonts.emoji.package;
+          name = cfg.fonts.emoji.name;
+        };
+      };
+      
+      polarity = cfg.polarity;
+    };
+  };
 }
