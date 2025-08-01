be9fe9d2abe006792878321559eff54375778f7c
Cody <acodywright@gmail.com>
2025-08-01 01:04:30 -0400

    disko configuration modules

---
+++ modules/nixos/system/disk/default.nix
@@ -0,0 +1,61 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.system.disk;
+in
+{
+  options.${namespace}.system.disk = {
+    enable = mkBoolOpt false "Enable disko disk management";
+    
+    type = mkOption {
+      type = types.enum [ "simple" "btrfs" "btrfs-impermanence" "btrfs-luks-impermanence" ];
+      default = "simple";
+      description = "Type of disk configuration to use";
+    };
+    
+    device = mkOption {
+      type = types.str;
+      default = "/dev/sda";
+      description = "Disk device to configure";
+    };
+    
+    withSwap = mkBoolOpt false "Enable swap partition";
+    
+    swapSize = mkOption {
+      type = types.int;
+      default = 8;
+      description = "Swap size in GB";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Import the appropriate disk configuration based on type
+    disko.devices = 
+      if cfg.type == "simple" then
+        (import ./simple-disk.nix { inherit lib; device = cfg.device; }).disko.devices
+      else if cfg.type == "btrfs" then
+        (import ./btrfs-disk.nix { inherit lib; device = cfg.device; withSwap = cfg.withSwap; swapSize = cfg.swapSize; }).disko.devices
+      else if cfg.type == "btrfs-impermanence" then
+        (import ./btrfs-impermanence-disk.nix { inherit lib config; device = cfg.device; withSwap = cfg.withSwap; swapSize = cfg.swapSize; }).disko.devices
+      else if cfg.type == "btrfs-luks-impermanence" then
+        (import ./btrfs-luks-impermanence-disk.nix { 
+          inherit lib config; 
+          device = cfg.device; 
+          withSwap = cfg.withSwap;
+          swapSize = cfg.swapSize;
+        }).disko.devices
+      else
+        throw "Unknown disk type: ${cfg.type}";
+    
+    # Add additional packages for LUKS configurations
+    environment.systemPackages = mkIf (cfg.type == "btrfs-luks-impermanence") [
+      pkgs.yubikey-manager
+    ];
+  };
+}
