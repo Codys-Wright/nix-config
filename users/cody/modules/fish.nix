0fe64f92f7cbae16ffc3449cc0923d43e4799756
Cody Wright <acodywright@gmail.com>
2025-12-22 07:42:24 +0000

    refactor(fish): consolidate fish config from user dots to global module
    Move all fish shell configuration from users/cody/dots/_fish/ into the
    global FTS fish module at modules/coding/shell/fish.nix:
    
    - Shell abbreviations: tool replacements (exa, bat, rg, fd), jj/git
      shortcuts, and magit bindings
    - Shell aliases: yazi, eza listings, vim pickers, editor shortcuts
    - Functions: jj helpers, emacs/spc wrappers, nix search helpers,
      cargo watch commands, and tv completion widget
    - Interactive init: ctrl+l unbind for zellij, tab binding for tv
      completion, bun global bin path
    
    Simplify user fish module to only set vi key bindings, avoiding
    duplication with the global module.

--- users/cody/modules/fish.nix
+++ users/cody/modules/fish.nix
@@ -1,28 +1,13 @@
-{inputs, ...}: {
+# User-specific fish overrides (if any)
+# Most fish configuration is now in FTS.coding._.shells._.fish
+{...}: {
   cody.fish.homeManager = {
-    pkgs,
-    lib,
     ...
-  }: let
-    # Path to the _fish directory
-    fishDir = ../dots/_fish;
-  in {
-    # Link tv.fish completion file
-    home.file.".config/fish/conf.d/tvtab.fish".source = "${fishDir}/tv.fish";
-
-    # Augment fish config - don't set enable, just add user-specific configuration
-    # This will merge with FTS.fish configuration
+  }: {
     programs.fish = {
-      # Import functions, aliases, and abbrs from _fish directory
-      functions = import "${fishDir}/functions.nix" {inherit inputs lib;};
-      shellAliases = import "${fishDir}/aliases.nix";
-      shellAbbrs = import "${fishDir}/abbrs.nix";
-
-      # Enable vi key bindings
+      # Enable vi key bindings on shell init
       shellInit = ''
         fish_vi_key_bindings
-        # Unbind Ctrl+l (default clear binding) to allow it to pass through to Zellij
-        bind -e \cl
       '';
     };
   };
