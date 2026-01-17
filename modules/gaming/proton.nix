d259b49a9372ebed1bd053a14d0cb4f63b2e8905
Cody Wright <acodywright@gmail.com>
2026-01-17 23:49:54 +0000

    feat: Add gaming tools (protontricks, gale)
    - Add protontricks and dotnet-runtime_6 to proton module
    - Add gale mod manager to r2modman module

--- modules/gaming/proton.nix
+++ modules/gaming/proton.nix
@@ -3,25 +3,38 @@
   FTS.apps._.gaming._.proton = {
     description = "Proton compatibility tools for running Windows games on Linux";
 
-    homeManager = {
-      pkgs,
-      lib,
-      ...
-    }:
+    homeManager =
+      {
+        pkgs,
+        lib,
+        ...
+      }:
       lib.mkIf (!pkgs.stdenv.isDarwin) {
-        home.packages = [pkgs.protonup-ng];
+        home.packages = [
+          pkgs.protonup-ng
+          pkgs.protonup-rs # Install/update GE-Proton and Wine-GE
+          pkgs.protontricks # Run winetricks commands for Proton games
+          pkgs.dotnet-runtime_6 # Required for MelonLoader on Il2Cpp games
+        ];
         home.sessionVariables = {
           STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
         };
       };
 
-    nixos = {pkgs, ...}: {
-      environment.systemPackages = [pkgs.protonup-ng pkgs.protonup-rs];
+    nixos =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [
+          pkgs.protonup-ng
+          pkgs.protonup-rs # Install/update GE-Proton and Wine-GE
+          pkgs.protontricks # Run winetricks commands for Proton games
+          pkgs.dotnet-runtime_6 # Required for MelonLoader on Il2Cpp games
+        ];
 
-      # AFTER THIS OPTION IS SET, RUN PROTONUP
-      environment.sessionVariables = {
-        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+        # AFTER THIS OPTION IS SET, RUN PROTONUP
+        environment.sessionVariables = {
+          STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+        };
       };
-    };
   };
 }
--- modules/gaming/r2modman.nix
+++ modules/gaming/r2modman.nix
@@ -1,22 +1,24 @@
 # r2modman mod manager aspect
-{
-  FTS,
-  ...
-}:
-{
+{FTS, ...}: {
   FTS.apps._.gaming._.r2modman = {
     description = "r2modman mod manager for Thunderstore mods";
 
-    homeManager =
-      { pkgs, lib, ... }:
+    homeManager = {
+      pkgs,
+      lib,
+      ...
+    }:
       lib.mkIf (!pkgs.stdenv.isDarwin) {
-        home.packages = [ pkgs.r2modman ];
+        home.packages = [pkgs.r2modman pkgs.gale];
       };
 
-    nixos =
-      { pkgs, lib, ... }:
+    nixos = {
+      pkgs,
+      lib,
+      ...
+    }:
       lib.mkIf (!pkgs.stdenv.isDarwin) {
-        environment.systemPackages = [ pkgs.r2modman ];
+        environment.systemPackages = [pkgs.r2modman pkgs.gale];
       };
   };
 }
