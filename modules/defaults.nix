4d836d514eaf56b53b0e120406301b99bb03b65b
Cody Wright <acodywright@gmail.com>
2026-01-17 23:50:00 +0000

    fix: Allow insecure dotnet-runtime-6 for MelonLoader
    Permit EOL dotnet-runtime-6.0.36 package required by MelonLoader
    for Il2Cpp game modding compatibility.

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -15,6 +15,14 @@
     nixos.nixpkgs.config.allowUnfree = true;
     darwin.nixpkgs.config.allowUnfree = true;
     homeManager.nixpkgs.config.allowUnfree = true;
+
+    # Allow EOL .NET 6.0 for MelonLoader compatibility
+    nixos.nixpkgs.config.permittedInsecurePackages = [
+      "dotnet-runtime-6.0.36"
+    ];
+    homeManager.nixpkgs.config.permittedInsecurePackages = [
+      "dotnet-runtime-6.0.36"
+    ];
   };
 }
 
