56fe0c40268a3f7be5ccf5b9fea7fa61c0aa7dc6
Cody Wright <acodywright@gmail.com>
2026-02-09 01:08:16 -0800

    feat: .flake-c1t.7 - US-007: Migrate user definitions to use den.lib.parametric angle bracket syntax

--- modules/coding/shell/user-shell.nix
+++ modules/coding/shell/user-shell.nix
@@ -1,43 +1,8 @@
-{ den,
-  FTS, ... }:
-let
-
-  description = ''
-    Sets a user default shell, enables the shell at OS and Home level.
-
-    Usage:
-
-      FTS.vic.includes = [
-        # will always love red snappers.
-        (den._.user-shell "fish")
-      ];
-  '';
-
-  userShell =
-    shell: user:
-    let
-      nixos =
-        { pkgs, ... }:
-        {
-          programs.${shell}.enable = true;
-          users.users.${user.userName}.shell = pkgs.${shell};
-        };
-      darwin = nixos;
-      homeManager.programs.${shell}.enable = true;
-    in
-    {
-      inherit nixos darwin homeManager;
-    };
-
-in
 {
-  den.provides.user-shell =
-    shell:
-    den.lib.parametric {
-      inherit description;
-      includes = [
-        ({ user, ... }: userShell shell user)
-        ({ home, ... }: userShell shell home)
-      ];
-    };
+  FTS,
+  ...
+}:
+{
+  # Backward-compatible alias to the user namespace shell provider.
+  FTS.coding._.user-shell = FTS.user._.shell;
 }
--- modules/user/shell.nix
+++ modules/user/shell.nix
@@ -1,17 +1,41 @@
 {
+  den,
   lib,
+  __findFile,
   ...
 }:
+let
+  description = ''
+    Sets a user default shell and enables the shell at OS and Home levels.
+  '';
+
+  userShell = shell: user: {
+    nixos =
+      { pkgs, ... }:
+      {
+        programs.${shell}.enable = true;
+        users.users.${user.userName}.shell = pkgs.${shell};
+      };
+
+    darwin =
+      { pkgs, ... }:
+      {
+        programs.${shell}.enable = true;
+        users.users.${user.userName}.shell = pkgs.${shell};
+      };
+
+    homeManager.programs.${shell}.enable = true;
+  };
+in
 {
   # Set default shell for user
-  # Usage: (FTS.user._.shell { default = "fish"; })
+  # Usage: (<FTS.user/shell> { default = "fish"; })
   FTS.user._.shell.__functor =
     _self:
     {
       default ? "fish",
       ...
-    }@args:
-    { user, ... }:
+    }:
     let
       availableShells = [
         "fish"
@@ -19,29 +43,18 @@
         "bash"
         "nushell"
       ];
+
       _ =
         if !(builtins.elem default availableShells) then
           throw "shell: unknown shell '${default}'. Available: ${builtins.concatStringsSep ", " availableShells}"
         else
           null;
     in
-    {
-      nixos =
-        { pkgs, ... }:
-        {
-          # Enable the shell system-wide
-          programs.${default}.enable = true;
-          # Set as the user's default shell
-          users.users.${user.userName}.shell = pkgs.${default};
-        };
-      darwin =
-        { pkgs, ... }:
-        {
-          # Enable the shell on Darwin
-          programs.${default}.enable = true;
-          # Set as the user's default shell
-          users.users.${user.userName}.shell = pkgs.${default};
-        };
+    <den.lib.parametric> {
+      inherit description;
+      includes = [
+        ({ user, ... }: userShell default user)
+        ({ home, ... }: userShell default home)
+      ];
     };
 }
-
