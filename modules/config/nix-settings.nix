8afab819d43f3cd594c57267d802820936835dd1
Cody Wright <acodywright@gmail.com>
2026-03-24 13:40:24 -0700

    Add Cachix substituters and @admin to trusted-users

--- modules/config/nix-settings.nix
+++ modules/config/nix-settings.nix
@@ -18,9 +18,17 @@ let
           trusted-users = [
             "root"
             "@wheel"
+            "@admin"
           ];
           substituters = [
             "https://cache.nixos.org/"
+            "https://devenv.cachix.org"
+            "https://fasttrackstudio.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+            "fasttrackstudio.cachix.org-1:r7v7WXBeSZ7m5meL6w0wttnvsOltRvTpXeVNItcy9f4="
           ];
         };
         gc = lib.optionalAttrs config.nix.enable {
