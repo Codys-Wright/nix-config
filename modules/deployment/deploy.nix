5b3ce55b464ce9a30dc7baaa0f93c4741767b8bf
Cody Wright <acodywright@gmail.com>
2026-04-06 14:36:38 -0700

    feat(deploy): add FTS.deploy aspect, wire up THEBATTLESHIP
    - New FTS.deploy parametric aspect: simple deploy-rs target definition
    - THEBATTLESHIP now deployable at 100.74.250.99 (tailscale) via
      `just deploy THEBATTLESHIP`
    - Separate from FTS.deployment (bootstrap/beacon machinery)
    - deploy-rs.nix auto-discovers hosts with deployment.ip set

---
+++ modules/deployment/deploy.nix
@@ -0,0 +1,53 @@
+# Simple deploy-rs target aspect
+# Defines NixOS options that deploy-rs.nix auto-discovers.
+# Any host that includes this becomes deployable via `just deploy <host>`.
+#
+# Usage:
+#   (FTS.deploy { ip = "100.74.250.99"; })
+#   (FTS.deploy { ip = "192.168.0.106"; sshUser = "root"; })
+#   (FTS.deploy { ip = "10.0.0.1"; sshPort = 2222; })
+{
+  FTS,
+  lib,
+  ...
+}:
+{
+  FTS.deploy = {
+    description = "Deploy-rs target: makes a host remotely deployable via `just deploy <host>`";
+
+    __functor =
+      _self:
+      {
+        ip,
+        sshPort ? 22,
+        sshUser ? "root",
+        ...
+      }:
+      {
+        nixos =
+          { lib, ... }:
+          {
+            options.deployment = {
+              enable = lib.mkEnableOption "deploy-rs deployment" // {
+                default = true;
+              };
+              ip = lib.mkOption {
+                type = lib.types.str;
+                default = ip;
+                description = "IP address for deploy-rs to target";
+              };
+              sshPort = lib.mkOption {
+                type = lib.types.port;
+                default = sshPort;
+                description = "SSH port for deploy-rs";
+              };
+              sshUser = lib.mkOption {
+                type = lib.types.str;
+                default = sshUser;
+                description = "SSH user for deploy-rs";
+              };
+            };
+          };
+      };
+  };
+}
