972b80548849b7ea2730130ab10b3ebdb973e802
Cody Wright <acodywright@gmail.com>
2026-04-06 21:07:55 -0700

    feat(flatpak): auto-add Flathub remote on activation

--- modules/apps/flatpak/flatpak.nix
+++ modules/apps/flatpak/flatpak.nix
@@ -2,10 +2,15 @@
 { fleet, ... }:
 {
   fleet.apps._.flatpaks = {
-    description = "Flatpak runtime with Flathub";
+    description = "Flatpak runtime with Flathub repository";
 
     nixos = _: {
       services.flatpak.enable = true;
+
+      # Add Flathub remote on activation
+      system.activationScripts.flathub = ''
+        ${"/run/current-system/sw/bin/flatpak"} remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || true
+      '';
     };
 
     homeManager =
