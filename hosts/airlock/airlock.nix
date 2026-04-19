279f737936fd77a45d887cf980cfc731cbfeaed6
Cody Wright <acodywright@gmail.com>
2026-04-19 02:38:59 -0700

    airlock: disable nix.enable in darwin aspect

--- hosts/airlock/airlock.nix
+++ hosts/airlock/airlock.nix
@@ -1,14 +1,25 @@
-{ inputs, ... }:
+{
+  inputs,
+  fleet,
+  lib,
+  __findFile,
+  ...
+}:
 
 {
 
   den.hosts.aarch64-darwin = {
     airlock = {
       description = "An Mac Mini that holds all the proprietary garbage that can't run on linux";
-      nix = {
-        enable = false;
-      };
     };
+  };
+
+  den.aspects.airlock = {
+    darwin =
+      { pkgs, ... }:
+      {
+        nix.enable = lib.mkDefault false;
+      };
   };
 
 }
