9fc345a9a603982f87beab6b2f12e08c6fadcd36
Cody Wright <acodywright@gmail.com>
2026-04-19 02:46:55 -0700

    Add deploy-darwin command and airlock deployment config

--- hosts/airlock/airlock.nix
+++ hosts/airlock/airlock.nix
@@ -11,6 +11,12 @@
   den.hosts.aarch64-darwin = {
     airlock = {
       description = "An Mac Mini that holds all the proprietary garbage that can't run on linux";
+      deployment = {
+        enable = true;
+        ip = "192.168.0.65";
+        sshPort = 22;
+        sshUser = "rat";
+      };
     };
   };
 
