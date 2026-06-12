dbf9edf0543d84b0e7e139a86995e2516e8eb1fa
Cody Wright <acodywright@gmail.com>
2026-06-12 02:07:12 -0700

    fix(gitops): readarr -> hotio fork (linuxserver+readarr upstream retired)

--- k8s/apps/wave3a.nix
+++ k8s/apps/wave3a.nix
@@ -29,7 +29,7 @@ let
     mkSimpleApp {
       inherit name port;
       host = "${sub}.starcommand.live";
-      image = if name == "readarr" then "lscr.io/linuxserver/readarr:nightly" else "lscr.io/linuxserver/${name}:latest";
+      image = if name == "readarr" then "ghcr.io/hotio/readarr:release" else "lscr.io/linuxserver/${name}:latest";
       state = {
         mountPath = "/config";
         size = "5Gi";
