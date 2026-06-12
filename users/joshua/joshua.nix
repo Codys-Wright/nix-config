1f731309c58998564d18281294d70f081c759eaf
Cody Wright <acodywright@gmail.com>
2026-06-11 17:24:56 -0700

    feat(joshua): drop timekpr; keep pam_time + curfew service

--- users/joshua/joshua.nix
+++ users/joshua/joshua.nix
@@ -193,36 +193,6 @@ in
           account   required      pam_time.so conffile=/etc/security/time.conf
         '';
 
-        services.timekpr = {
-          enable = true;
-          adminUsers = [ "cody" ];
-        };
-
-        systemd.services.timekpr-joshua-setup = {
-          description = "Configure Timekpr rules for Joshua";
-          # Pulled in by timekpr.service, NOT multi-user.target. Being
-          # WantedBy=multi-user.target forces an implicit Before=multi-user.target,
-          # but timekpr.service is itself After=multi-user.target and we run
-          # After=timekpr.service -> ordering cycle. Hang off timekpr.service
-          # instead so it still runs once the daemon is up, no target ordering.
-          wantedBy = [ "timekpr.service" ];
-          after = [ "timekpr.service" ];
-          requires = [ "timekpr.service" ];
-          serviceConfig = {
-            Type = "oneshot";
-          };
-          script = ''
-            ${pkgs.timekpr}/bin/timekpra --setalloweddays joshua '1;2;3;4;5;6;7'
-            ${pkgs.timekpr}/bin/timekpra --setallowedhours joshua ALL '8;9;10;11;12;13;14;15;16;17;18;19;20;21'
-            ${pkgs.timekpr}/bin/timekpra --setlockouttype joshua terminate
-            ${pkgs.timekpr}/bin/timekpra --setplaytimeenabled joshua true
-            ${pkgs.timekpr}/bin/timekpra --setplaytimealloweddays joshua '1;2;3;4;5;6;7'
-            ${pkgs.timekpr}/bin/timekpra --setplaytimelimits joshua '3600;3600;3600;3600;3600;3600;3600'
-            ${pkgs.timekpr}/bin/timekpra --setplaytimeactivities joshua 'brave-browser[Brave Browser]'
-            ${pkgs.timekpr}/bin/timekpra --setplaytimeunaccountedintervalsflag joshua false
-          '';
-        };
-
         systemd.services.joshua-curfew = {
           description = "Terminate Joshua sessions outside allowed hours";
           serviceConfig = {
@@ -518,21 +488,6 @@ in
           name = "Virtual Machine Manager";
           noDisplay = true;
         };
-
-        # Timekpr client warnings for Joshua: 15m, 5m, and 1m remaining.
-        home.file.".config/timekpr/timekpr.conf".text = ''
-          [CONFIG]
-          LOG_LEVEL = 1
-          SHOW_LIMIT_NOTIFICATION = True
-          SHOW_ALL_NOTIFICATIONS = True
-          SHOW_SECONDS = True
-          USE_SPEECH_NOTIFICATIONS = False
-          NOTIFICATION_TIMEOUT = 4
-          NOTIFICATION_TIMEOUT_CRITICAL = 8
-          USE_NOTIFICATION_SOUNDS = False
-          NOTIFICATION_LEVELS = 900[3];300[2];60[1]
-          PLAYTIME_NOTIFICATION_LEVELS = 900[3];300[2];60[1]
-        '';
 
         # Default Joshua into Plasma on SDDM.
         home.file.".dmrc".text = ''
